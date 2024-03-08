123b7166d54596dab712fcce7925a7d130021538
ChouCookieSan <choucookiesan@gmail.com>
2024-03-08 03:31:03 +0100

    Initial commit

---
+++ config.lua
@@ -0,0 +1,232 @@
+Config = {}
+
+Config.Models = { -- Any TV Models used on the map or in locations must be defined here.
+    [`des_tvsmash_start`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085, 
+        Offset = vector3(-1.02, -0.055, 1.04)
+    },
+    [`prop_flatscreen_overlay`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085, 
+        Offset = vector3(-1.02, -0.055, 1.04)
+    },
+    [`prop_laptop_lester2`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085, 
+        Offset = vector3(-1.02, -0.055, 1.04)
+    },
+    [`prop_monitor_02`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085, 
+        Offset = vector3(-1.02, -0.055, 1.04)
+    },
+    [`prop_trev_tv_01`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085, 
+        Offset = vector3(-1.02, -0.055, 1.04)
+    },
+    [`prop_tv_02`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085, 
+        Offset = vector3(-1.02, -0.055, 1.04)
+    },
+    [`prop_tv_03_overlay`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085, 
+        Offset = vector3(-1.02, -0.055, 1.04)
+    },
+    [`prop_tv_06`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085, 
+        Offset = vector3(-1.02, -0.055, 1.04)
+    },
+    [`prop_tv_flat_01`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085, 
+        Offset = vector3(-1.02, -0.055, 1.04)
+    },
+    [`prop_tv_flat_01_screen`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085, 
+        Offset = vector3(-1.02, -0.055, 1.04)
+    },
+    [`prop_tv_flat_02b`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085, 
+        Offset = vector3(-1.02, -0.055, 1.04)
+    },
+    [`prop_tv_flat_03`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085, 
+        Offset = vector3(-1.02, -0.055, 1.04)
+    },
+    [`prop_tv_flat_03b`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085, 
+        Offset = vector3(-1.02, -0.055, 1.04)
+    },
+    [`prop_tv_flat_michael`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085, 
+        Offset = vector3(-1.02, -0.055, 1.04)
+    },
+    [`prop_monitor_w_large`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085, 
+        Offset = vector3(-1.02, -0.055, 1.04)
+    },
+    [`prop_tv_03`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085, 
+        Offset = vector3(-1.02, -0.055, 1.04)
+    },
+    [`prop_tv_flat_02`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085, 
+        Offset = vector3(-1.02, -0.055, 1.04)
+    },
+    --[`v_ilev_cin_screen`] = {
+    --    DefaultVolume = 0.8,
+    --    Range = 20.0,
+    --    Target = "tvscreen", -- Only use if prop has render-target name.
+    --    Scale = 0.085, 
+    --    Offset = vector3(-1.02, -0.055, 1.04)
+    --},
+    [`apa_mp_h_str_avunitl_04`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085, 
+        Offset = vector3(-1.02, -0.055, 1.04)
+    },
+    [`apa_mp_h_str_avunitl_01_b`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085, 
+        Offset = vector3(-1.02, -0.055, 1.04)
+    },
+    [`apa_mp_h_str_avunitm_03`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085, 
+        Offset = vector3(-1.02, -0.055, 1.04)
+    },
+    [`ex_prop_ex_tv_flat_01`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085, 
+        Offset = vector3(-1.02, -0.055, 1.04)
+    },
+    [`apa_mp_h_str_avunits_01`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085, 
+        Offset = vector3(-1.02, -0.055, 1.04)
+    },
+    [`v_res_lest_monitor`] = {
+        DefaultVolume = 0.5,
+        Range = 20.0,
+        Target = "tvscreen", -- Only use if prop has render-target name.
+        Scale = 0.085, 
+        Offset = vector3(-1.02, -0.055, 1.04)
+    },
+    --[`prop_monitor_w_large`] = {
+    --    DefaultVolume = 0.5,
+    --    Range = 20.0,
+    --    Target = "tvscreen", -- Only use if prop has render-target name.
+    --    Scale = 0.085, 
+    --    Offset = vector3(-1.02, -0.055, 1.04)
+    --},
+    --[`prop_cs_tv_stand`] = {
+    --    DefaultVolume = 0.5,
+    --    Range = 20.0,
+    --    Target = "tvscreen", -- Only use if prop has render-target name.
+    --    Scale = 0.085, 
+    --    Offset = vector3(-1.02, -0.055, 1.04)
+    --},
+    --[`prop_monitor_03b`] = {
+    --    DefaultVolume = 0.5,
+    --    Range = 20.0,
+    --    Target = "tvscreen", -- Only use if prop has render-target name.
+    --    Scale = 0.085, 
+    --    Offset = vector3(-1.02, -0.055, 1.04)
+    --},
+    --[`ex_prop_monitor_01_ex`] = {
+    --    DefaultVolume = 0.5,
+    --    Range = 20.0,
+     --   Target = "tvscreen", -- Only use if prop has render-target name.
+     --   Scale = 0.085, 
+     --   Offset = vector3(-1.02, -0.055, 1.04)
+    --},
+}
+
+Config.Locations = { -- REMOVE ALL IF NOT USING ONESYNC, OR IT SHALL BREAK.
+    {
+        Model = `prop_tv_flat_01`,
+        Position = vector4(144.3038, -1037.4647, 29.4173, 70.81),
+    },
+}
+
+Config.Channels = { -- These channels are default channels and cannot be overriden.
+    {name = "Twitch | ChouCookieSan", url = "twitch.tv/choucookiesan"},
+    {name = "OFF", url = "youtube.com/shorts/x79C_PwCmnU?feature=share"},
+}
+
+Config.BannedWords = {
+    --"google",
+}
+
+Config.Events = { -- Events for approving broadcasts / interactions (due to popular demand).
+    ScreenInteract = function(source, data, key, value, cb) -- cb() to approve. 
+        if value.url then 
+            for i=1, #Config.BannedWords do 
+                if string.find(value.url, Config.BannedWords[i]) then 
+                    return
+                end
+            end
+        end
+        cb()
+    end,    
+    Broadcast = function(source, data, cb)  -- cb() to approve. 
+        cb()
+    end,
+}
